9aca0b0ecce2b0b877d41ccdfc9ac9c6d80425b6
Gyalog Bence <gyalog.bence_2020c@bolyai-szakkozep.hu>
2024-04-18 10:35:56 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Eredmeny1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridViewPeople = new System.Windows.Forms.DataGridView();
+            this.openFileDialogPeople = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.comboBoxProfession = new System.Windows.Forms.ComboBox();
+            this.labelProfession = new System.Windows.Forms.Label();
+            this.textBoxEmail2 = new System.Windows.Forms.TextBox();
+            this.labelEmail2 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPeople)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(1067, 28);
+            this.menuStripMain.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Exit";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.infoToolStripMenuItem.Text = "info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // dataGridViewPeople
+            // 
+            this.dataGridViewPeople.AllowUserToAddRows = false;
+            this.dataGridViewPeople.AllowUserToDeleteRows = false;
+            this.dataGridViewPeople.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridViewPeople.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPeople.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewPeople.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewPeople.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewPeople.Name = "dataGridViewPeople";
+            this.dataGridViewPeople.ReadOnly = true;
+            this.dataGridViewPeople.RowHeadersWidth = 51;
+            this.dataGridViewPeople.Size = new System.Drawing.Size(708, 526);
+            this.dataGridViewPeople.TabIndex = 1;
+            this.dataGridViewPeople.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPeople_CellContentClick);
+            // 
+            // openFileDialogPeople
+            // 
+            this.openFileDialogPeople.FileName = "*.txt";
+            this.openFileDialogPeople.Filter = "Text files|*.txt";
+            // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 28);
+            this.splitContainerMain.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainerMain.Name = "splitContainerMain";
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainerMain.Panel1.Controls.Add(this.comboBoxProfession);
+            this.splitContainerMain.Panel1.Controls.Add(this.labelProfession);
+            this.splitContainerMain.Panel1.Controls.Add(this.textBoxEmail2);
+            this.splitContainerMain.Panel1.Controls.Add(this.labelEmail2);
+            this.splitContainerMain.Panel1.Controls.Add(this.textBoxEmail);
+            this.splitContainerMain.Panel1.Controls.Add(this.labelEmail);
+            this.splitContainerMain.Panel1.Controls.Add(this.textBoxLastName);
+            this.splitContainerMain.Panel1.Controls.Add(this.labelLastName);
+            this.splitContainerMain.Panel1.Controls.Add(this.textBoxFirstName);
+            this.splitContainerMain.Panel1.Controls.Add(this.labelFirstName);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.dataGridViewPeople);
+            this.splitContainerMain.Size = new System.Drawing.Size(1067, 526);
+            this.splitContainerMain.SplitterDistance = 354;
+            this.splitContainerMain.SplitterWidth = 5;
+            this.splitContainerMain.TabIndex = 2;
+            // 
+            // comboBoxProfession
+            // 
+            this.comboBoxProfession.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxProfession.FormattingEnabled = true;
+            this.comboBoxProfession.Items.AddRange(new object[] {
+            "",
+            "doctor",
+            "firefighter",
+            "police officer",
+            "worker"});
+            this.comboBoxProfession.Location = new System.Drawing.Point(85, 132);
+            this.comboBoxProfession.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxProfession.Name = "comboBoxProfession";
+            this.comboBoxProfession.Size = new System.Drawing.Size(160, 24);
+            this.comboBoxProfession.TabIndex = 9;
+            // 
+            // labelProfession
+            // 
+            this.labelProfession.AutoSize = true;
+            this.labelProfession.Location = new System.Drawing.Point(5, 135);
+            this.labelProfession.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelProfession.Name = "labelProfession";
+            this.labelProfession.Size = new System.Drawing.Size(43, 16);
+            this.labelProfession.TabIndex = 8;
+            this.labelProfession.Text = "Tárgy";
+            this.labelProfession.Click += new System.EventHandler(this.labelProfession_Click);
+            // 
+            // textBoxEmail2
+            // 
+            this.textBoxEmail2.Location = new System.Drawing.Point(85, 100);
+            this.textBoxEmail2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxEmail2.Name = "textBoxEmail2";
+            this.textBoxEmail2.Size = new System.Drawing.Size(160, 22);
+            this.textBoxEmail2.TabIndex = 7;
+            // 
+            // labelEmail2
+            // 
+            this.labelEmail2.AutoSize = true;
+            this.labelEmail2.Location = new System.Drawing.Point(5, 103);
+            this.labelEmail2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelEmail2.Name = "labelEmail2";
+            this.labelEmail2.Size = new System.Drawing.Size(62, 16);
+            this.labelEmail2.TabIndex = 6;
+            this.labelEmail2.Text = "Százalék";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(85, 68);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(160, 22);
+            this.textBoxEmail.TabIndex = 5;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(5, 71);
+            this.labelEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(73, 16);
+            this.labelEmail.TabIndex = 4;
+            this.labelEmail.Text = "Érdemjegy";
+            this.labelEmail.Click += new System.EventHandler(this.labelEmail_Click);
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(85, 36);
+            this.textBoxLastName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(160, 22);
+            this.textBoxLastName.TabIndex = 3;
+            this.textBoxLastName.TextChanged += new System.EventHandler(this.textBoxLastName_TextChanged);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(5, 39);
+            this.labelLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(69, 16);
+            this.labelLastName.TabIndex = 2;
+            this.labelLastName.Text = "Last name";
+            this.labelLastName.Click += new System.EventHandler(this.labelLastName_Click);
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(85, 4);
+            this.textBoxFirstName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(160, 22);
+            this.textBoxFirstName.TabIndex = 1;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(4, 7);
+            this.labelFirstName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(69, 16);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "First name";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.splitContainerMain);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Reporting";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPeople)).EndInit();
+            this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel1.PerformLayout();
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridViewPeople;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPeople;
+        private System.Windows.Forms.SplitContainer splitContainerMain;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.ComboBox comboBoxProfession;
+        private System.Windows.Forms.Label labelProfession;
+        private System.Windows.Forms.TextBox textBoxEmail2;
+        private System.Windows.Forms.Label labelEmail2;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eredmeny1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelProfession_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelLastName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GYB");
+        }
+
+        private void labelEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPeople_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogPeople.ShowDialog() == DialogResult.OK)
+            {
+                EredmenyRepository.Path = openFileDialogPeople.FileName;
+                dataGridViewPeople.DataSource = EredmenyRepository.FindAll();
+            }
+        }
+    }
+}
